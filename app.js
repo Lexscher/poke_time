0082c090d00700560edede0b511731674fc9fd64
Alexander Schelchere <ascellie@gmail.com>
2019-07-04 12:07:13 -0400

    app renders index and show

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 // Require dependencies
-const express = require('express')
-const axios = require('axios')
+const express = require('express');
+const fetch = require('node-fetch');
 
 // Call express
-const app = express()
+const app = express();
 
 // Set our server
 const PORT = 3000
 
-// Just show something random on our home page
+// set view engine to ejs
+app.set('view engine', 'ejs');
+
+// set url endpoints
+    // url for all pokemon
+const urlForAll = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+    // url for one pokemon
+const urlForOne = `https://pokeapi.co/api/v2/pokemon/`
+
+// Array to store our data
+let pokeList = [];
+
+// fetch the url
+fetch(urlForAll)
+    // parse into json 
+    .then(response => response.json())
+    // Get the results and assign them to our pokeList
+    .then(jsonData => pokeList = jsonData.results)
+    // Handle the error
+    .catch(err => console.log(err));
+
+
+// Home Page
 app.get('/', (request, response) => {
-    response.send("Hello Poke-World!")
+    response.send(`Hello Poke-World!`)
+});
+
+// INDEX PAGE - show all pokemon 
+app.get('/pokemons', (request, response) => {
+    response.render('index', {pokemons: pokeList})
+});
+
+// SHOW PAGE - show one pokemon
+app.get('/pokemons/:id', (request, response) => {
+    response.render('show')
 });
 
 
